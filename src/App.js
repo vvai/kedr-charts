589574cb9faaa9753ad796623ac908fdf789d16e
Vitalik Vojtehovsky <vojtehovsky@gmail.com>
2021-08-31 08:06:16 +0300

    add sample chart

--- src/App.js
+++ src/App.js
@@ -1,22 +1,47 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import { Chart } from '@antv/g2';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [data, setData] = useState([
+    { genre: 'Sports', sold: 275 },
+    { genre: 'Strategy', sold: 115 },
+    { genre: 'Action', sold: 120 },
+    { genre: 'Shooter', sold: 350 },
+    { genre: 'Other', sold: 150 },
+  ]);
+
+  useEffect(() => {
+    const chart = new Chart({
+      container: 'chart',
+      width: 600,
+      height: 300,
+    });
+    chart.data(data);
+    chart.interval().position('genre*sold');
+    chart.render();
+  });
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <div className="charts">
+          <div id="chart"></div>
+        </div>
+        <p>Sample chart</p>
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        {/* <p>
           Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
+        </p> */}
+        {/* <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
       </header>
     </div>
   );
